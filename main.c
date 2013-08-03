5f8251cecb8ce18d36931c28ff6feaef0b8e94cc
Astro <astro@spaceboyz.net>
2013-08-04 00:11:09 +0200

    2 pics

--- main.c
+++ main.c
@@ -23,21 +23,41 @@
 #define false 0
 
 
+unsigned char pics[2][8] = {
+  { 0b11011011,
+    0b10010010,
+    0b10010010, 
+    0b10010010,
+    0b10010010,
+    0b10010010,
+    0b10010010,
+    0b11011011 },
+  { 0b10000001,
+    0b01000010,
+    0b01111110, 
+    0b01011010,
+    0b01111110,
+    0b01111110,
+    0b01100110,
+    0b00111100 },
+};
+
 void main(void) {
-        canvasInit();
-        int x=0;
-        int color=1;
-        while(true) {
-                // 5ms delay (see delay.h for reference)
-                _mdelay(5);
+  canvasInit();
+  int pic=0;
+  while(true) {
+    for(int y = 0; y < 10; y++)
+      for(int x = 0; x < 12; x++) {
+        // 5ms delay (see delay.h for reference)
+        _mdelay(5);
 
-                // set led on or off
-                setLedXY(x,0,color);
+        int color = 0;
+        if (y >= 1 && y < 9 &&
+            x >= 2 && x < 10)
+          color = (pics[pic][y - 1] >> (9 - x)) & 1;
+        setLedXY(x, y, color);
+      }
 
-                x++;
-                if (x > 119) {
-                        x=0;
-                        color=!color;
-                }
-        }
+    pic = (pic + 1) % 2;
+  }
 }
